986d19ebde2a314c2987c75269641ddb74ee3ce1
diduk001 <didurenkos@gmail.com>
2024-04-21 20:36:24 +0300

    fixed output, added determinant calculation

--- Matrix.hpp
+++ Matrix.hpp
@@ -63,6 +63,20 @@ public:
         }
     }
 
+    Matrix(size_t newRows, size_t newCols, T **newElements) {
+        if (newRows <= 0 || newCols <= 0)
+            throw std::invalid_argument("Rows and newCols must be positive numbers");
+        this->rows = newRows;
+        this->cols = newCols;
+        this->data = new T *[newRows];
+        for (int rowIdx = 0; rowIdx < newRows; rowIdx++) {
+            this->data[rowIdx] = new T[newCols];
+            for (int colIdx = 0; colIdx < newCols; colIdx++)
+                this->data[rowIdx][colIdx] = newElements[rowIdx][colIdx];
+        }
+    }
+
+
     static Matrix<T> identity(size_t size, T value = 1) {
         Matrix<T> result(size);
         for (size_t i = 0; i < size; i++)
@@ -236,6 +250,13 @@ public:
             data[row][j] *= value;
     }
 
+    void divideRow(size_t row, const T &value) {
+        if (row >= rows)
+            throw std::out_of_range("Index out of range");
+        for (size_t j = 0; j < cols; j++)
+            data[row][j] /= value;
+    }
+
     void addRow(size_t row1, size_t row2, const T &value) {
         if (row1 >= rows || row2 >= rows)
             throw std::out_of_range("Index out of range");
@@ -246,32 +267,161 @@ public:
             data[row1][j] += data[row2][j] * value;
     }
 
-    // Determinant
-    // TODO: implement
-    T determinant() const {
+    // Find matrix determinant
+    // Convert into diagonal matrix (like in Gauss method) and return product of all diagonal elements
+    // https://cp-algorithms.com/linear_algebra/determinant-gauss.html
+    T det() const {
+        if (this->getRows() != this->getCols())
+            throw std::invalid_argument("Matrix must be square");
 
+        const size_t size = this->getCols();
+        if (size == 2) {
+            T a = this->get(0, 0),
+                    b = this->get(0, 1),
+                    c = this->get(1, 0),
+                    d = this->get(1, 1);
+            return a * d - b * c;
+        }
+
+        Matrix mtx(*this);
+        T result = 1;
+        for (size_t stepIdx = 0; stepIdx < size; stepIdx++) {
+            // Choose row with maximal absolute value of stepIdx_th element
+            size_t maxByAbsRowIdx = stepIdx;
+            T maxByAbsValue = mtx.get(maxByAbsRowIdx, stepIdx);
+            for (size_t maxByAbsRowCandidate = stepIdx + 1; maxByAbsRowCandidate < size; maxByAbsRowCandidate++) {
+                T candidateValue = std::abs(mtx.get(maxByAbsRowCandidate, stepIdx));
+                if (candidateValue > std::abs(maxByAbsValue)) {
+                    maxByAbsRowIdx = maxByAbsRowCandidate;
+                    maxByAbsValue = mtx.get(maxByAbsRowIdx, stepIdx);
+                }
+            }
+            if (std::abs(maxByAbsValue) == 0)
+                return 0;
+
+            // Swap current step row and max abs row
+            if (stepIdx != maxByAbsRowIdx)
+                result = -result;
+            mtx.swapRows(stepIdx, maxByAbsRowIdx);
+
+            // Divide max abs row by first stepIdx element
+            result *= maxByAbsValue;
+            mtx.divideRow(stepIdx, maxByAbsValue);
+
+            // Nullify all elements under [stepIdx][stepIdx]
+            for (size_t lowerRowIdx = stepIdx + 1; lowerRowIdx < size; lowerRowIdx++) {
+                if (std::abs(mtx.get(lowerRowIdx, stepIdx)) == 0)
+                    continue;
+                T coefficient = -mtx.get(lowerRowIdx, stepIdx);
+                mtx.addRow(lowerRowIdx, stepIdx, coefficient);
+            }
+        }
+
+        return result;
     }
 
-    // Inverse
-    // TODO: implement
-    Matrix<T> operator!() const;
+    // Inverse matrix
+    // Convert into identity matrix (like in Gauss method) and do same operation on identity matrix
+    // https://cp-algorithms.com/linear_algebra/linear-system-gauss.html
+    // Returns 0 matrix if matrix is not invertible
+    Matrix operator!() const {
+        if (this->getRows() != this->getCols())
+            throw std::invalid_argument("Matrix must be square");
+
+        const size_t size = this->getCols();
+        const T det = this->det();
+        if (det == 0)
+            return {size, size};
+
+        if (size == 2) {
+            T a = this->get(0, 0),
+                    b = this->get(0, 1),
+                    c = this->get(1, 0),
+                    d = this->get(1, 1);
+
+            T **resultElements;
+            resultElements = new T *[2];
+            resultElements[0] = new T[2]{d, -b};
+            resultElements[1] = new T[2]{-c, a};
+
+            return Matrix(2, 2, data) / det;
+        }
+
+        Matrix mtx = Matrix(*this);
+        Matrix result = Matrix::identity(size);
+
+        for (size_t stepIdx = 0; stepIdx < size; stepIdx++) {
+            // Choose row with maximal absolute value of stepIdx_th element
+            size_t maxByAbsRowIdx = stepIdx;
+            T maxByAbsValue = mtx.get(maxByAbsRowIdx, stepIdx);
+            for (size_t maxByAbsRowCandidate = stepIdx + 1; maxByAbsRowCandidate < size; maxByAbsRowCandidate++) {
+                T candidateValue = std::abs(mtx.get(maxByAbsRowCandidate, stepIdx));
+                if (candidateValue > std::abs(maxByAbsValue)) {
+                    maxByAbsRowIdx = maxByAbsRowCandidate;
+                    maxByAbsValue = mtx.get(maxByAbsRowIdx, stepIdx);
+                }
+            }
+
+            // Swap current step row and max abs row
+            mtx.swapRows(stepIdx, maxByAbsRowIdx);
+            result.swapRows(stepIdx, maxByAbsRowIdx);
+
+            // Divide max abs row by first stepIdx element
+            mtx.multiplyRow(stepIdx, 1 / maxByAbsValue);
+            result.multiplyRow(stepIdx, 1 / maxByAbsValue);
+
+            // Nullify all elements under [stepIdx][stepIdx]
+            for (size_t lowerRowIdx = 0; lowerRowIdx < size; lowerRowIdx++) {
+                if (lowerRowIdx == stepIdx || std::abs(mtx.get(lowerRowIdx, stepIdx)) == 0) {
+                    continue;
+                }
+                double coefficient = -mtx.get(lowerRowIdx, stepIdx);
+                mtx.addRow(lowerRowIdx, stepIdx, coefficient);
+                result.addRow(lowerRowIdx, stepIdx, coefficient);
+            }
+        }
+
+        return result;
+    }
+
 };
 
 
 // Input and Output
-// TODO: implement
 template<typename T>
-std::istream &operator>>(std::istream &in, Matrix<T> &mtx);
+std::istream &operator>>(std::istream &in, Matrix<T> &mtx) {
+    size_t rows, cols;
+    in >> rows >> cols;
+    mtx = Matrix<T>(rows, cols);
+    for (size_t i = 0; i < mtx.getRows(); i++)
+        for (size_t j = 0; j < mtx.getCols(); j++) {
+            T value;
+            in >> value;
+            mtx.set(i, j, value);
+        }
+    return in;
+}
 
 template<typename T>
 std::ostream &operator<<(std::ostream &out, const Matrix<T> &mtx) {
-    std::array<size_t, 2> shape = mtx.shape();
-    out << shape[0] << " " << shape[1] << std::endl;
-    for (size_t i = 0; i < shape[0]; i++) {
-        for (size_t j = 0; j < shape[1]; j++)
-            out << mtx.get(i, j) << " ";
+    const size_t rows = mtx.getRows();
+    const size_t cols = mtx.getCols();
+
+    out << "[" << std::endl;
+    for (size_t rowIdx = 0; rowIdx < rows; rowIdx++) {
+        out << "\t[";
+        for (size_t colIdx = 0; colIdx < cols; colIdx++) {
+            out << "\t" << mtx.get(rowIdx, colIdx);
+            if (colIdx < cols - 1)
+                out << ",";
+        }
+        out << "]";
+        if (rowIdx < rows - 1)
+            out << ",";
         out << std::endl;
     }
+    out << "]" << std::endl;
+
     return out;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,22 +1,58 @@
-//
-// Created by Stepan Didurenko on 20.04.2024.
-//
-
 #include "Matrix.hpp"
 #include <iostream>
+#include <fstream>
 
 int main() {
-    Matrix<int> m(3, 3);
-    m.set(0, 0, 1);
-    m.set(0, 1, 2);
-    m.set(0, 2, 3);
-    m.set(1, 0, 4);
-    m.set(1, 1, 5);
-    m.set(1, 2, 6);
-    m.set(2, 0, 7);
-    m.set(2, 1, 8);
-    m.set(2, 2, 9);
-    m = m * 3;
-    std::cout << m;
+    // mtx1 = {{8, 4, 5}, {8, 5, 7}, {0, 0, 7}}
+    std::string inputFilename;
+    std::cout << "Matrix input filename > ";
+    std::cin >> inputFilename;
+    std::ifstream in(inputFilename);
+
+    Matrix<double> mtx1;
+    in >> mtx1;
+
+    Matrix mtx2 = Matrix(mtx1);
+    mtx2.swapRows(0, 1);
+
+    Matrix mtx3 = Matrix(mtx1);
+    mtx3.multiplyRow(1, 4);
+
+    Matrix mtx4 = Matrix(mtx1);
+    mtx4.addRow(2, 0, 3);
+    Matrix mtx5 = !mtx1;
+
+    Matrix mtx6 = Matrix(mtx1).transpose();
+
+    std::string outputFilename;
+    std::cout << "Matrix output filename > ";
+    std::cin >> outputFilename;
+    std::ofstream out(outputFilename);
+
+    out << "Original matrix (A):" << '\n';
+    out << mtx1 << std::endl;
+
+    out << "Swapped rows 0 and 1" << std::endl;
+    out << mtx2 << std::endl;
+
+    out << "Multiplied row 1 by 4" << std::endl;
+    out << mtx3 << std::endl;
+
+    out << "Added row 0 multiplied by 3 to row 2" << std::endl;
+    out << mtx4 << std::endl;
+
+    out << "Original matrix determinant: " << mtx1.det() << std::endl;
+    out << "Inverse original matrix (I)" << std::endl;
+    out << mtx5 << std::endl;
+
+    out << "A * I = " << std::endl;
+    out << mtx1 * mtx5 << std::endl;
+
+    out << "I * A = " << std::endl;
+    out << mtx5 * mtx1 << std::endl;
+
+    out << "A^T" << std::endl;
+    out << mtx6 << std::endl;
+
     return 0;
 }
